306111cb74ff18a51fc60dc94f0d5814416849f5
Sentinel1980 <sentinael1980@yahoo.com>
2020-03-13 04:18:25 -0400

    Fixes
    Fixed some text output, made the script more user friendly.

--- Aria2.sh
+++ Aria2.sh
@@ -2,14 +2,14 @@
 
 # This is a script for Aria2 downloader. The purpose is to set up some common settings and wait for user input of a url to download.
 
-read -p 'URL to download, Master?': url;
-read -p 'Location to save, Master?': dir;
+read -p 'URL to download?': url;
+read -p 'Location to save?': dir;
 
 
 
 aria2c -x8 -k1M $url -d$dir &
 
-echo 'As you will, Master'
+echo 'End'
 
 bash
 
